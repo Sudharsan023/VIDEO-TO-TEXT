d85bd44afbbb75acd87882888177a9877445e13b
sudharsan <sudharsan.m23@gmail.com>
2023-03-27 21:49:48 +0530

    homepage created

---
+++ pages/.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="border-white">
+        <textarea class="w-24 h-24"></textarea>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-auto h-screen text-center p-16 "><div class="bg-black m-8 p-12 pt-24  items-center">
+        <div class="flex justify-center">
+        <img src="/ourlogo.png" class="w-20">
+        <h1 class="w-50 text-white italic font-bold md:text-6xl text-3xl ml-11 mr-11 m-auto text-center">VIDEO SUMMARIZER</h1></div>
+        <form action="/action_page.php">
+            <div class="text-center mt-11 md:text-2xl text-xl p-12"><input type="url" id="homepage" name="homepage" placeholder="  Paste a video URL..." class="w-200 border-0 rounded-sm items-center p-2 bg-white text-black"></div>
+            <div class="text-center p-1"><input type="submit" class="bg-sky-500 hover:bg-blue-700 p-2 rounded-lg items-center mt-11 text-white shadow-lg hover-black shadow-blue-500/50 text-xl"></div>
+
+        </form>
+        </div>
+
+    </div>
+    <div class="h-52 m-12 text-white">
+        <div class="text-white"><p class="italic text-2xl p-2">How to summarizer your youtube video?</p><p class="text-xl p-1">     .....Its not easy to summarize  videos from youtube, video summarizer could help you to summarize from youtube videos without any fee.you are in the right placve to summarize youtube videos , follow the below steps to summarize youtube videos on your device(PC,Android,iPhone or MAC) and learn how to use our video summarizer.</p></div>
+        <div class="p-5"></div>
+        <div><p class="italic text-2xl">Step by Step to summarize Youtube videos with video summarizer.</p><p class="text-xl p-2">Step 1 : Visit youtube.com .click on the share button and copy the Youtube video URL which contains the video.</p><div class="flex"><img src="https://imgs.search.brave.com/sWxneUAiLoABU6faiYyZZ2dmPeuo-Mj-zMUOcJDeIeo/rs:fit:1024:574:1/g:ce/aHR0cHM6Ly9qYWdz/d2VidGVrLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMjAyMS8w/My9Zb3VUdWJlX1Zp/ZGVvX1NoYXJlX09w/dGlvbnNfZGlhbG9n/LTEwMjR4NTc0LnBu/Zw" class="w-76 h-56"></div><p class="text-xl p-2">Step 2:Open video summarizer & Paste the URL into the URL box and press the "Submit" button</p><div class="flex "><img class="w-84 h-80 items-center p-3 pb-24" src="/front.jpg"></div><p></p></div>
+    </div>
+</template>
